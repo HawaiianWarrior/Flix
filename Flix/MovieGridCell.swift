dd8cf2e5b2175c1a466a1dba6469a6131242705f
Brayden Featheran <featheran@cox.net>
2021-02-08 18:55:14 -0800

    Assignment 1 part 2 add-ons and collective grid

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by user188190 on 2/8/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
